379a9736617c680915197fe7000483c45c8fd750
Oseias Isisdoro <oseiasisidoro@gmail.com>
2022-07-09 23:46:58 -0300

    create RentsFactory.php

---
+++ database/factories/RentsFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Rents;
+use Faker\Generator as Faker;
+
+$factory->define(Rents::class, function (Faker $faker) {
+    return [
+        'user_id' => factory('App\Models\User')->create()->id,
+        'customer_id' => factory('App\Models\Customers')->create()->id,
+        'product_id' => factory('App\Models\Products')->create()->id,
+        'lease_start_date' => $faker->dateTime->getTimestamp(),
+        'lease_end_date' => $faker->dateTime->getTimestamp(),
+        'additional_charge' => $faker->numberBetween(23, 45),
+        'maintenance_cost' => $faker->numberBetween(23, 45),
+        'damage_rate' => $faker->numberBetween(23, 45),
+        'cost' => $faker->numberBetween(23, 45),
+        'status' => $faker->randomElement(['late', 'in_progress', 'finished']),
+    ];
+});
